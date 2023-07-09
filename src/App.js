d806db2a6adb4ccbb7751f0518cd794bc4a706e5
ndilanwil <nuadjedidi@gmail.com>
2023-07-09 20:52:59 +0100

    Starting Home page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {Login} from "./pages/Login/Login"
 import {SignUp} from "./pages/SignUp/SignUp"
-// import {Home} from "./pages/Home/Home"
+import {Home} from "./pages/Home/Home"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />}/>
         <Route path="/signup" element={<SignUp />}/>
       </Routes>
